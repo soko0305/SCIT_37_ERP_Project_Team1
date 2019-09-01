771d1b149dc734ecd138bcade98843de88910a55
harachoi <haraandyou@gmail.com>
2019-09-01 17:09:28 +0900

    20190901 1709 커밋커밋~

---
+++ ERP_Project/src/main/java/com/project/erp/controller/LinkController.java
@@ -0,0 +1,19 @@
+package com.project.erp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@Controller
+public class LinkController {
+
+	
+	@RequestMapping(value="/haraManagement", method = RequestMethod.GET)
+	public String Management1(){
+		return "management1";
+	}
+
+}
