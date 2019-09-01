9c4520bb2deacc962d04488c647d4ee114e7ef78
soko0305 <soko0305@gmail.com>
2019-09-01 19:20:10 +0900

    전영배 최신화

--- ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String home(){
-		return "index";
+		return "signup";
 	}
 
 	@RequestMapping(value="/header", method = RequestMethod.GET)
