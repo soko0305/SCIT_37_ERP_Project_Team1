a1d6abb336df32962ef740e7688684f98e5f8ff8
soko0305 <soko0305@gmail.com>
2019-09-02 18:38:54 +0900

    form 및  ui 추가

--- ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String home(){
-		return "inventory/confirmInventory";
+		return "main/index";
 		}
  
  
