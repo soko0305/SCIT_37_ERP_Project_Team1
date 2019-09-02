1d1de120e68ae0348194b3f2269b568c83a58d3d
soko0305 <soko0305@gmail.com>
2019-09-02 11:34:19 +0900

    no message

---
+++ ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.project.erp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String home(){
+		return "main/index";
+		}
+ 
+ 
+	@RequestMapping(value="/header", method = RequestMethod.GET)
+	public String header(){
+		return "main/header";
+	}
+	
+	@RequestMapping(value="/navbar", method = RequestMethod.GET)
+	public String navbar(){
+		return "main/navbar";
+	}
+	
+	@RequestMapping(value="/drawerWrapper", method = RequestMethod.GET)
+	public String drawerWrapper(){
+		return "main/drawerWrapper";
+	}
+	
+	@RequestMapping(value="/wrapperFooter", method = RequestMethod.GET)
+	public String wrapperFooter(){
+		return "main/wrapperFooter";
+	}
+	
+	@RequestMapping(value="/setting", method = RequestMethod.GET)
+	public String setting(){
+		return "main/setting";
+	}
+	
+}
