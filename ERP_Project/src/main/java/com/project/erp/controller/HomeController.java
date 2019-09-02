852fbcdda68742c174f6a4b18d4ad7f6d2d8ee75
soko0305 <soko0305@gmail.com>
2019-09-02 19:32:51 +0900

    오류 기입 란 추가

--- ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String home(){
 
-		return "main/index";
+		return "inventory/updateInventory";
 		}
 
 
