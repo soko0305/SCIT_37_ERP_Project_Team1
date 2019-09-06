0ee8aba19dab34c31f5f55e65e9598f158f1ec18
soko0305 <soko0305@gmail.com>
2019-09-06 09:18:47 +0900

    db, 파이어베이스 관련 파일 및 내용 추가 및 창고 관련 ui 수정

---
+++ ERP_Project/src/main/resources/static/firebase/firebase-config.js
@@ -0,0 +1,18 @@
+(function($) {
+
+  "use strict"; // Start of use strict
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+		    apiKey: "AIzaSyB8_jsBI65na3fjm9RiBSZyeWXZUlAjfso",
+		    authDomain: "scit37team1project.firebaseapp.com",
+		    databaseURL: "https://scit37team1project.firebaseio.com",
+		    projectId: "scit37team1project",
+		    storageBucket: "scit37team1project.appspot.com",
+		    messagingSenderId: "672782150568",
+		    appId: "1:672782150568:web:5ffaf4f154fb7cdba6cb9d"
+		  };
+		  // Initialize Firebase
+		  firebase.initializeApp(firebaseConfig);
+
+	
+})(jQuery); // End of use strict
