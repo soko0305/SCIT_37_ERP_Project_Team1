b5b77af53318a09b46fb6d2852540d02a47e4821
soko0305 <soko0305@gmail.com>
2019-09-07 23:37:43 +0900

    주석 추가

--- ERP_Project/src/main/java/com/project/erp/controller/InventoryController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/InventoryController.java
@@ -18,6 +18,7 @@ public class InventoryController {
 	InventoryService inventoryService;
 	int flag = 0;
 	
+	//모든 창고 리스트 가져오는 메소드
 	@RequestMapping(value="/selectAllInventory", method = RequestMethod.GET)
 	public @ResponseBody ArrayList<WarehouseVO> selectAllInventory(){
 		ArrayList<WarehouseVO> wList = null;
@@ -25,17 +26,20 @@ public class InventoryController {
 		return wList;
 	}
 	
+	//창고 추가 메소드
 	@RequestMapping(value="/insertWarehouse", method = RequestMethod.POST)
 	public @ResponseBody void insertWarehouse(WarehouseVO warehouse){
 		flag=0;
 		flag = inventoryService.insertWarehouse(warehouse);
 	}
 	
+	//추가, 최신화 성공 여부 확인 메소드
 	@RequestMapping(value="/checkSuccess", method = RequestMethod.GET)
 	public @ResponseBody int checkSuccess(){
 		return flag;
 	}
 	
+	//추가 후 새로 추가된 창고 정보 가져오는 메소드
 	@RequestMapping(value="/selectLatestWarehouse", method = RequestMethod.GET)
 	public @ResponseBody WarehouseVO selectLatestWarehouse(){
 		WarehouseVO w = null;
@@ -43,12 +47,14 @@ public class InventoryController {
 		return w;
 	}
 	
+	//창고 정보 업데이트 메소드
 	@RequestMapping(value="/updateWarehouse", method = RequestMethod.POST)
 	public @ResponseBody void updateWarehouse(WarehouseVO warehouse){
 		flag=0;
 		flag = inventoryService.updateWarehouse(warehouse);
 	}
 	
+	//창고 정보 삭제 메소드
 	@RequestMapping(value="/deleteWarehouse", method = RequestMethod.GET)
 	public @ResponseBody int deleteWarehouse(String warehouse_code){
 		int check =0;
@@ -56,6 +62,7 @@ public class InventoryController {
 		return check;
 	}
 	
+	//warehouse_code통해 창고 정보 검색하는 메소드
 	@RequestMapping(value="/selectWarehouseByCode", method = RequestMethod.GET)
 	public @ResponseBody WarehouseVO selectWarehouseByCode(String warehouse_code){
 		WarehouseVO w = null;
