6670fc62452736a71716f3c9d97ba11da60c98e4
harachoi <harauniverse@naver.com>
2019-09-08 17:57:46 +0900

    no message

--- ERP_Project/src/main/java/com/project/erp/controller/SalesController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/SalesController.java
@@ -13,17 +13,24 @@ import com.google.gson.JsonParser;
 import com.project.erp.dao.SalesDAO;
 import com.project.erp.vo.Buyer;
 import com.project.erp.vo.Sales;
+import com.project.erp.vo.SalesDetail;
 
 @Controller
 public class SalesController {
 	
 	@Autowired SalesDAO dao;
 
+	//엑셀을 직접 업로드 했을 때, SALES와 SALESDETAIL을 DB에 저장시키는 메소드.
 	@RequestMapping(value="/insertData", method = RequestMethod.POST)
 	public @ResponseBody String insertData(@RequestBody String excelJson){
 		
 		String sales_ordernum = null;
 		String company = null;
+		String salesdetail_name = null;
+		String pd_code = null;
+		String salesdetail_amount = null;
+		String salesdetail_price = null;
+		
 		int totalPrice = 0;
 		
 		JsonParser jsonParser = new JsonParser();
@@ -44,35 +51,38 @@ public class SalesController {
 				System.out.println();
 			}
 			
+			salesdetail_name = object.get("salesdetail_name").getAsString();
+			pd_code = object.get("pd_code").getAsString();
+			salesdetail_amount = object.get("salesdetail_amount").getAsString();
+			salesdetail_price = object.get("salesdetail_price").getAsString();
 			
-			String salesdetail_name = object.get("salesdetail_name").getAsString();
-			String pd_code = object.get("pd_code").getAsString();
-			String salesdetail_amount = object.get("salesdetail_amount").getAsString();
-			String salesdetail_price = object.get("salesdetail_price").getAsString();
-			
+			//totalPrice 계산.
 			totalPrice += Integer.parseInt(salesdetail_price);
 			
-			System.out.println(company);
-			System.out.println(sales_ordernum);
-			System.out.println(salesdetail_name);
-			System.out.println(pd_code);
-			System.out.println(salesdetail_price);
-			System.out.println(salesdetail_amount);
+			//SalesDetail 객체를 DB에 저장하는 메소드.
+			SalesDetail salesDetail = new SalesDetail();
+			salesDetail.setSalesdetail_name(salesdetail_name);
+			salesDetail.setSalesdetail_amount(salesdetail_amount);
+			salesDetail.setSalesdetail_price(salesdetail_price);
+			salesDetail.setSales_ordernum(sales_ordernum);
+			salesDetail.setPd_code(pd_code);
+			
 			
 			}
 		
 		String price = Integer.toString(totalPrice);
 		
-
+		//회사이름으로 바이어 시퀀스 불러오기.
 		Buyer buyer = new Buyer();
 		buyer.setBuyer_corp(company);
 		Buyer buyer1 = dao.SelectOneBuyer(buyer);
 		
-
+		//Sales 객체를 DB에 저장하는 메소드.
 		Sales sales = new Sales();
 		sales.setBuyerseq(buyer1.getBuyerseq());
 		sales.setSales_totalprice(price);
 		dao.insertSales(sales);
+		
 		
 		
 
