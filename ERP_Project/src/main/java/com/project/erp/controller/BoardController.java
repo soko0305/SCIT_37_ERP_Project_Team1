47aa219fce47ba1454e618bf3322fb1c5f6796c8
whitebear <whitebear5741@gmail.com>
2019-09-10 08:35:05 +0900

    풀하기 위한 커밋

---
+++ ERP_Project/src/main/java/com/project/erp/controller/BoardController.java
@@ -0,0 +1,59 @@
+package com.project.erp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.erp.dao.BoardDAO;
+ import com.project.erp.vo.BoardVO;
+ 
+@Controller
+public class BoardController {
+
+	@Autowired
+	BoardDAO bDAO;
+
+	// 원자재 등록
+	@RequestMapping(value = "/boardInsert", method = RequestMethod.POST)
+	public String boardInsert(BoardVO board) {
+
+		int result = bDAO.boardInsert(board);
+
+		if (result == 0) {
+			return "/member/board";
+		} else {
+			return "/member/board";
+		}
+	}
+
+	// 원자재 삭제
+	@RequestMapping(value = "/boardDelete", method = RequestMethod.POST)
+	public String boardDelete(BoardVO board) {
+		
+		int result = bDAO.boardDelete(board);
+
+		if (result == 0) {
+			return "/member/board";
+		} else {
+			return "/member/board";
+		}
+	}
+
+	// 원자재 수정
+	@ExceptionHandler
+	@RequestMapping(value = "/boardUpdate", method = RequestMethod.POST)
+	public String boardUpdate(BoardVO board) {
+
+		int result = bDAO.boardUpdate(board);
+
+		if (result == 0) {
+			return "/member/board";
+		} else {
+			return "/member/board";
+		}
+	}
+}
+
+
