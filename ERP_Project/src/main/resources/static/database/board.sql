1532accffd1810dcfc9fa89ced13de4c6dc25b88
whitebear <whitebear5741@gmail.com>
2019-09-10 00:05:29 +0900

    sql추가해서 커밋

---
+++ ERP_Project/src/main/resources/static/database/board.sql
@@ -0,0 +1,25 @@
+ 
+drop table erp_notice;
+
+create table erp_notice(
+board_seq  varchar2(100) primary key,
+userid not null references erp_member(userid),
+title varchar2(100) not null,
+content varchar2(1000) not null,
+time date default sysdate not null
+)
+drop  sequence board_seq;
+ create sequence board_seq;
+ 
+ drop table erp_file;
+ 
+create table erp_file(
+file_seq varchar2(100) primary key,
+boared_seq not null references erp_notice(board_seq),
+org_name varchar2(100) not null,
+sav_name varchar2(100) not null,
+time date default sysdate not null
+)
+drop  sequence file_seq;
+ create sequence file_seq;
+  
---
+++ ERP_Project/src/main/resources/static/database/production.sql
@@ -0,0 +1,36 @@
+
+ drop table PRODUCT;
+
+create table PRODUCT(
+PD_CODE  varchar2(100) primary key,
+PD_NAME varchar2(100) not null,
+PD_RMCOST   number not null,
+PD_MANUFCOST   number not null
+)
+ 
+drop table MANUFACTURE;
+
+create table MANUFACTURE(
+MFSEQ varchar2(100) primary key,
+MMCODE varchar2(100) references PRODUCT(PD_CODE) not null,
+MANUF_STARTDATE date not null,
+MANUF_ENDDATE  date not null,
+MANUF_STATUS varchar2(100) not null,
+MANUF_WAREHOUSE varchar2(100) not null,
+MANUF_AMOUNT number not null,
+SALES_ORDERNUM varchar2(100) not null,
+MANUF_REQUSERID varchar2(100) not null,
+MANUF_REQDATE  date not null,
+SALESDETAILSEQ varchar2(100) not null
+)
+drop  sequence MFSEQ;
+ create sequence MFSEQ;
+
+drop table PRODUCT_MATERIAL;
+
+create table PRODUCT_MATERIAL(
+PRODUCT_MATERIAL_CODE  varchar2(100) primary key,
+PD_CODE  varchar2(100) references PRODUCT(PD_CODE) not null,
+RAWM_CODE  varchar2(100) not null,
+PRODUCT_MATERIAL_AMOUNT  number not null
+)
