45cf62cb356960235f4d32c876402546503d85e0
whitebear <whitebear5741@gmail.com>
2019-09-10 20:17:47 +0900

    일중씨꺼 풀하기 위한 커밋

--- ERP_Project/src/main/java/com/project/erp/vo/ProductVO.java
+++ ERP_Project/src/main/java/com/project/erp/vo/ProductVO.java
@@ -9,13 +9,8 @@ public class ProductVO {
 	private String pd_name; 
 	private String pd_rmcost;  
 	private String pd_manufcost; 
-<<<<<<< HEAD
-	
-=======
 	private String pd_sort;
 	private String warehouse_code;
-	private String systemamount;
->>>>>>> 01444f176e94867eab19ac7f0ba9a87142e4cfa2
-	
+
 }
  
