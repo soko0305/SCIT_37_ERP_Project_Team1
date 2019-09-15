ce0fa6d2917f47a77baee4d9e114cc6e01b6a408
harachoi <harauniverse@naver.com>
2019-09-15 19:29:45 +0900

    ㅎㅎㅎㅎ

--- ERP_Project/src/main/java/com/project/erp/controller/LinkController.java
+++ ERP_Project/src/main/java/com/project/erp/controller/LinkController.java
@@ -45,8 +45,7 @@ public class LinkController {
 		return "sales/salesHistory";
 	}
 	@RequestMapping(value="/insertBuyer", method = RequestMethod.GET)
-	public String insertBuyer(Model model){
-		
+	public String insertBuyer(){	
 		return "sales/insertBuyer";
 	}
 	
@@ -62,7 +61,7 @@ public class LinkController {
 	public String realTimeOfferDetail(SalesDetail salesDetail, Model model){
 		List<SalesDetail> list = service.selectSalesDetailByOrdernum(salesDetail);
 		model.addAttribute("salesDetailList", list);
-		return "sales/offersheet";
+		return "sales/realTimeOfferDetail";
 	}
 	
 	@RequestMapping(value="/salesHistoryDetail", method=RequestMethod.GET)
