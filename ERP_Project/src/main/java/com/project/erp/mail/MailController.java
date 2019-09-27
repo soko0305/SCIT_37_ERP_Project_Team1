3b3248168220db03b84f9ed8fc085f158160f368
harachoi <haraandyou@gmail.com>
2019-09-27 09:22:47 +0900

    no message

---
+++ ERP_Project/src/main/java/com/project/erp/mail/MailController.java
@@ -0,0 +1,8 @@
+package com.project.erp.mail;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MailController {
+
+}
--- ERP_Project/src/main/java/com/project/erp/service/ProductionService.java
+++ ERP_Project/src/main/java/com/project/erp/service/ProductionService.java
@@ -101,6 +101,8 @@ public class ProductionService {
 		return pDAO.statusProduce(manufacture);
 
 	}
+
+
  
 	}
  
